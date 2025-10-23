82f819d1b443ec6943921256983582b3ddccd8e6
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2025-10-23 10:40:52 -0700

    feat: Add Image and Tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,32 @@
 const markdownIt = require("markdown-it");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("style.css");
-  eleventyConfig.addPassthroughCopy("script.js");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
+
+  eleventyConfig.addPassthroughCopy("src/assets/");
+
+
 
   eleventyConfig.addPairedNunjucksShortcode("markdown", (content) => {
     return markdownIt().render(content);
+  });
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", async (src, alt, widths, sizes) => {
+    let metadata = await Image(src, {
+      widths: widths || [null],
+      formats: ["webp", "jpeg"],
+      outputDir: "./_site/img/",
+      urlPath: "/img/",
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
   });
 
   return {
